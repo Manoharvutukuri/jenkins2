6f7170cfe94e3555b439c6ec747f535006e3756d
Manoharvutukuri <100669585+Manoharvutukuri@users.noreply.github.com>
2022-03-03 16:44:56 +0530

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -2,5 +2,6 @@ public class Ex1{
  public static void main(String[] args){
   System.out.println("hii...Jenkins");
     System.out.println("hii..Manohar");
+    System.out.println("hii..Manohar");
 }
 }
